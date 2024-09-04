0c279e318a0e4e2d488c59bc13050bda0e79d890
bigstth~ <sitthidsn@gmail.com>
2024-09-05 00:59:24 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import { useState } from 'react'
+
+function App() {
+    const [token, setToken] = useState('')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const playGame = async () => {
+        setIsLoading(true)
+        const headers = {
+            accept: 'application/json, text/plain, */*',
+            authorization: token,
+            origin: 'https://telegram.blum.codes',
+        }
+
+        try {
+            const response = await axios.post('https://game-domain.blum.codes/api/v1/game/play', {}, { headers })
+            const gameId = response?.data?.gameId
+
+            setTimeout(async () => {
+                try {
+                    const points = Math.floor(Math.random() * (225 - 200 + 1)) + 200
+
+                    const response2 = await axios.post('https://game-domain.blum.codes/api/v1/game/claim', { gameId, points }, { headers })
+
+                    alert(response2?.data, ` +${points}`)
+                    setIsLoading(false)
+                } catch (error) {
+                    setIsLoading(false)
+                }
+            }, 41000)
+        } catch (error) {
+            setIsLoading(false)
+            alert('token หมดอายุ ไปเอาอันใหม่มา')
+        }
+    }
+
+    return (
+        <>
+            <div style={{ display: 'flex', gap: '4px' }}>
+                <input onChange={(e) => setToken(e.target.value)} />
+                <button onClick={() => playGame()} disabled={isLoading}>
+                    run
+                </button>
+            </div>
+            {isLoading && <h1>รอแปป</h1>}
+        </>
+    )
+}
+
+export default App
